5701c3d80e0b5046cf629a09ad585e995386a6d1
luis <luis96raul1@gmail.com>
2021-12-11 18:24:06 -0500

    functional solution

---
+++ app.rb
@@ -0,0 +1,58 @@
+puts "Inserta tu texto para validarlo:"
+value = gets.chomp
+
+# filter for (, ), :) or :(
+def filter(text)
+  counter = 0
+  stack = []
+  dotsCache = ''
+  text.split('').each do |char|
+    counter += 1 if dotsCache == ':'
+    if char == ':'
+      dotsCache = char
+      counter = 1 
+    end
+    ['(',')'].each do |sample|
+      if char == sample
+        stack<<(dotsCache+char)
+      end
+    end
+    dotsCache = '' if counter == 2
+  end
+  return stack
+end
+# function to delete pairs of ()
+def deletePairs(currentStack,sample)
+  stack2 = []
+  currentStack.each do |element|
+    next if element == ':'
+    if sample[element]
+      index = stack2.rindex(sample[element])
+      if index 
+        stack2.delete_at(index)
+      else
+        stack2<<element
+      end
+    else
+      stack2 << element
+    end
+  end
+  return stack2
+end
+# function to check if the stack is valid
+def validate(currentStack)
+  return (currentStack.empty?||(!currentStack.include?('(')&&!currentStack.include?(')')))
+end
+
+stack = filter(value)
+balanced = false
+3.times do |i|
+  samples = [{')'=>'('},{':)'=>'('}, {')'=>':('}]
+  stack = deletePairs(stack,samples[i])
+  if validate(stack)
+    balanced = true
+    break
+  end
+end
+
+balanced ? puts("balanceado") : puts("desbalanceado")
